9820c64ec2330120e71b2f261821654878ae43e9
Raja-Nalliyappan <n.raja3710@gmail.com>
2025-12-18 22:25:59 +0530

    profile icon add

--- src/Components/Dashboard/Header.jsx
+++ src/Components/Dashboard/Header.jsx
@@ -1,6 +1,7 @@
 import "./Header.css"
 import logo from "../Dashboard/Dashboard-images/One-City-One-Pay-Logo.png"
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import loginprofile from "../Dashboard/loginprofile.gif"
 
 export const Header = () => {
 
@@ -15,21 +16,30 @@ export const Header = () => {
     }
   }
 
+  let loginUsername = JSON.parse(localStorage.getItem("user")).name;
+
   return (
     <>
-    <header className="header">
-      <div className="header-left">
-        <img src={logo} alt="Logo" className="logo" />
-      </div>
+      <header className="header">
+        <div className="header-left">
+          <img src={logo} alt="Logo" className="logo" />
+        </div>
 
-      <div className="header-center">
-        <h1>Plan Your Journey – Book Tickets and Travel with Ease</h1>
-      </div>
+        <div className="header-center">
+          <h1>Plan Your Journey – Book Tickets and Travel with Ease</h1>
+        </div>
 
-      <div className="header-right">
-        <a href="#" className="logout-btn" onClick={handleLogout}>Logout</a>
-      </div>
-    </header>
+        <div className="loginProfile">
+          <div className="dropdown"><img src={loginprofile} width={40} height={40} alt="loginprofile" className="dropbtn"/>
+            <div className="dropdown-content">
+              <p>{loginUsername}</p>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          </div>
+        </div>
+
+
+      </header>
     </>
   );
 };
